e94d8523919c18add2e0c626baa495ce6054860a
liu <532433365@qq.com>
2019-08-23 13:44:05 +0800

    feat: helloworld client

--- netty2_helloworld/src/com/qianliu/client/Cleint.java
+++ netty2_helloworld/src/com/qianliu/client/Cleint.java
@@ -1,44 +1,48 @@
-package com.qianliu.server;
+package com.qianliu.client;
 
-import org.jboss.netty.bootstrap.ServerBootstrap;
+
+import org.jboss.netty.bootstrap.ClientBootstrap;
 import org.jboss.netty.channel.*;
-import org.jboss.netty.channel.socket.nio.NioServerSocketChannelFactory;
+import org.jboss.netty.channel.socket.nio.NioClientSocketChannelFactory;
 import org.jboss.netty.handler.codec.string.StringDecoder;
 import org.jboss.netty.handler.codec.string.StringEncoder;
 
 import java.net.InetSocketAddress;
-
+import java.util.Scanner;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-public class Server {
+public class Cleint {
     public static void main(String[] args) {
+        ClientBootstrap bootstrap = new ClientBootstrap();
 
-        //开启一个服务类
-        ServerBootstrap bootstrap = new ServerBootstrap();
-
-        //boss线程监听端口，worker线程负责数据读写
         ExecutorService boss = Executors.newCachedThreadPool();
         ExecutorService work = Executors.newCachedThreadPool();
 
-        // 设置nio socket工厂：交代监听端口的boss，和读写事件的work
-        bootstrap.setFactory(new NioServerSocketChannelFactory(boss,work));
+        bootstrap.setFactory(new NioClientSocketChannelFactory(boss,work));
 
-        // 设置管道工厂: 可以在启动自定义管道，也可以使用系统自带的一些管道
         bootstrap.setPipelineFactory(new ChannelPipelineFactory() {
             @Override
             public ChannelPipeline getPipeline() throws Exception {
-                ChannelPipeline pipeline = Channels.pipeline();
+                ChannelPipeline pipeline  = Channels.pipeline();
                 pipeline.addLast("decoder", new StringDecoder()); // StringDecoder这个管道可以让我们直接在读取数据时的数据变成已经反序列化的String类型（系统默认读取的数据时Chennel）
                 pipeline.addLast("encoder", new StringEncoder()); // StringEncoder可以让我们回写数据的时候直接返回一个字符串
-                pipeline.addLast("helloHandler", new HelloHandler());
+                pipeline.addLast("helloHandler", new hiHandler());
                 return pipeline;
             }
         });
 
-        // 绑定消息端口
-        bootstrap.bind(new InetSocketAddress(10101));
+        //连接服务端
+        ChannelFuture channelFuture = bootstrap.connect(new InetSocketAddress("localhost",10101));
+        Channel channel = channelFuture.getChannel();
 
-        System.out.println("start!!!");
+        System.out.println("start..");
+
+        Scanner s = new Scanner(System.in);
+        while (true){
+            //System.out.println("请写入数据：");
+            channel.write(s.next());
+        }
+
     }
 }
---
+++ netty2_helloworld/src/com/qianliu/client/hiHandler.java
@@ -0,0 +1,60 @@
+package com.qianliu.client;
+
+import org.jboss.netty.channel.*;
+
+public class hiHandler extends SimpleChannelHandler{
+    /**
+     * 接收消息
+     */
+    @Override
+    public void messageReceived(ChannelHandlerContext ctx, MessageEvent e) throws Exception {
+
+        //ChannelBuffer s = (ChannelBuffer) e.getMessage();
+        //String s = new String(s.array());
+
+        // 因为pipeline中添加了StringDecoder，所以e.getMessage()可以直接强转为String，否则需要使用上面的代码;
+        String s = (String) e.getMessage();
+        System.out.println(s);
+
+        //回写数据
+        // 回写的数据字节是一个字符串，如果没有StringEncoder通道，需要String -> ChennalBuffer再发送
+        //ctx.getChannel().write("hi");
+        super.messageReceived(ctx, e);
+    }
+
+    /**
+     * 捕获异常
+     */
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, ExceptionEvent e) throws Exception {
+        System.out.println("exceptionCaught");
+        super.exceptionCaught(ctx, e);
+    }
+
+    /**
+     * 新连接
+     */
+    @Override
+    public void channelConnected(ChannelHandlerContext ctx, ChannelStateEvent e) throws Exception {
+        System.out.println("channelConnected");
+        super.channelConnected(ctx, e);
+    }
+
+    /**
+     * 必须是链接已经建立，关闭通道的时候才会触发
+     */
+    @Override
+    public void channelDisconnected(ChannelHandlerContext ctx, ChannelStateEvent e) throws Exception {
+        System.out.println("channelDisconnected");
+        super.channelDisconnected(ctx, e);
+    }
+
+    /**
+     * channel关闭的时候触发，如果连接失败。那么只会有channelClosed会触发，channelDisconnected不会触发
+     */
+    @Override
+    public void channelClosed(ChannelHandlerContext ctx, ChannelStateEvent e) throws Exception {
+        System.out.println("channelClosed");
+        super.channelClosed(ctx, e);
+    }
+}
